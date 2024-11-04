f3ebec700a7d6972a35c6dfd33c53f1d78a21d22
valery <tikavalery@gmail.com>
2024-11-04 14:07:07 -0800

    added jwt and bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,17 +3,21 @@ const router = express.Router()
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs")
-router.get("/", (req, res) =>{
-    res.send("Hello ")
-})
+const jwt = require("jsonwebtoken");
+const {JWT_SECRET} = require("../keys")
+const requireLogin = require("../middleware/requireLogin")
+
+
+// router.get("/protected",requireLogin,(req,res)=>{
+//     res.send("Hello user")
+// })
+
 
 router.post("/signup",(req,res)=>{
     const{name, email, password} = req.body
     if(!email || !password|| !name){
-   return res.status(422).json({error:"Please add all the fields"})
-       
+   return res.status(422).json({error:"Please add all the fields"})   
     } 
-
     console.log(name,email, password)
    User.findOne({email:email}).then((savedUser)=>{
     if(savedUser){
@@ -26,9 +30,6 @@ router.post("/signup",(req,res)=>{
             password:hashedpassword,
             name
         })
-    
-        console.log(user)
-    
         user.save().then(user =>{
             res.json({message:"Saved successfully"})
         }).catch(err =>{
@@ -41,5 +42,34 @@ router.post("/signup",(req,res)=>{
     console.log(err)
    })
 })
+
+router.post("/signin",(req,res)=>{
+       const {email,password} = req.body
+    if(!email || !password){
+        res.status(422).json({error:"Please add email or password"})
+    }
+    User.findOne({email:email}).then(savedUser =>{
+        if(!savedUser){
+          return res.status(422).json({error:"Invalid email or password"})
+                          }   
+                            bcrypt.compare(password,savedUser.password)
+            .then(doMatch =>{
+                if(doMatch){
+                  const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+                  res.json({token})
+                }else{
+                    return res.status(422).json({error:"Invalid password"})
+                }
+            }).catch(err =>{
+                console.log(err)
+            })
+    })
+    
+       
+})
+
+// router.post("/signin",(req,res)=>{
+
+// }).
 
 module.exports = router
