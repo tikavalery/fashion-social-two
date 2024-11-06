b87e58282609657efda0a4fdbc2794d1ba61bfa1
valery <tikavalery@gmail.com>
2024-11-06 11:27:41 -0800

    started to add the front end code

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NavBar = () =>{
+  return(
+    <nav>
+         <div className="nav-wrapper white">
+      <a href="#" className="brand-logo left">Logo</a>
+      <ul id="nav-mobile" className="right">
+        <li><a href="sass.html">Login</a></li>
+        <li><a href="badges.html">Signup</a></li>
+        <li><a href="collapsible.html">Profile</a></li>
+      </ul>
+    </div>
+    </nav>
+  )
+   
+ 
+}
+
+export default NavBar;
