b554bf3f395eb1954851c1788384e29e54b372ba
valery <tikavalery@gmail.com>
2025-02-13 23:02:31 -0800

    added changes to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React,{useContext}from "react";
 import {Link} from "react-router-dom"
+import {UserContext} from "../App"
 
-const NavBar = () =>{
+const NavBar = () =>{ 
+  const {state, dispatch} = useContext()
+  const renderList = () =>{
+    if(state){
+          return[
+      <>
+      <li><Link to = "/profile">Profile</Link></li>
+      <li><Link to = "/create">Create Post</Link></li>
+      </>
+    ]
+    } else{
+      return[
+        <>
+          <li> <Link to = "/signin"> SignIn</Link></li>
+          <li> <Link to = "/signup">Signup</Link></li>
+        </>
+      ]
+    }
+
+  }
   return(
     <nav>
          <div className="nav-wrapper white">
       <Link to="/" className="brand-logo left">  Fashion Social</Link>
       <ul id="nav-mobile" className="right">
-        <li><Link to="/signin"> Login</Link></li>
-        <li><Link to="/signup"> Signup</Link></li>
-        <li><Link to="/profile"> Profile</Link></li>
-        <li>
-          <Link to="/create"> Create Post</Link>
-        </li>
+          {renderList}
       </ul>
     </div>
     </nav>
