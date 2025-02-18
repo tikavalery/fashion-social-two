9df39156bde6411cac5b507479032c128554210b
valery <tikavalery@gmail.com>
2025-02-17 17:09:17 -0800

    added some changes to signin page

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -48,6 +48,7 @@ router.post("/signup",(req,res)=>{
 
 router.post("/signin",(req,res)=>{
        const {email,password} = req.body
+    
     if(!email || !password){
         res.status(422).json({error:"Please add email or password"})
     }
